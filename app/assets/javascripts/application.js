decb5c9975c086cae4ca7bdb803cf2fa65aaf53e
devpram <winztech@gmail.com>
2019-10-04 08:16:58 +0530

    Installing bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
+//= require popper
 //= require rails-ujs
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
 
